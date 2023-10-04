1d60a45bf9810664e66153217d7001a9f249057c
Navod Peiris <navodpeiris1234@gmail.com>
2023-10-04 21:11:21 +0530

    added

--- mlapi/resample.py
+++ mlapi/resample.py
@@ -3,7 +3,7 @@ import librosa.display
 import soundfile as sf
 
 # Define the input and output file paths
-input_file = 'noise.wav'
+input_file = 'mama_navod.wav'
 output_file = 'output_16k.wav'
 
 # Load the audio file
